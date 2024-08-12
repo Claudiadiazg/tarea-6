7cad38f984e7380b73938f75e08927f6ff56ffce
Claudia Diaz G <cldiazg@creasistemas.com>
2024-08-12 15:14:39 -0500

    Tarea 6

---
+++ src/assets/components/titulo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function  Title(props) {
+    // recuerda que este componente es funcional
+    // se deben incluir estilos para hacerlo mas atractivo
+    return <h1>{props.title}</h1>;
+
+
+
+}
+export default Title;
